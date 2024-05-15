9e4e13629bdf2d45b301c300590aa2b30a237bb4
pengu <blizzspace@gmail.com>
2024-05-15 01:31:55 -0400

    Test bound generation

--- src/bsdata.c
+++ src/bsdata.c
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-  int numberOfRandomNumbers = 10000;
+  int numberOfRandomNumbers = 10;
   int *listOfRandomNumbers; 
 
   // seed RNG
   initializeGenerator();
 
+
+
+
   listOfRandomNumbers = generateUnboundedNumberOfIntegers(numberOfRandomNumbers);
+  if (!listOfRandomNumbers) return 1;
+
+  printIntegerList(listOfRandomNumbers, numberOfRandomNumbers);
+
+  free(listOfRandomNumbers);
+
+  listOfRandomNumbers = generateUpperBoundedNumberOfIntegers(numberOfRandomNumbers, 5);
+  if (!listOfRandomNumbers) return 1;
+
+  printIntegerList(listOfRandomNumbers, numberOfRandomNumbers);
+
+  free(listOfRandomNumbers);
+
+  listOfRandomNumbers = generateLowerBoundedNumberOfIntegers(numberOfRandomNumbers, 5);
   if (!listOfRandomNumbers) return 1;
 
   printIntegerList(listOfRandomNumbers, numberOfRandomNumbers);
