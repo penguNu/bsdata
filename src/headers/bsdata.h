b11f8fe1e0e0e1dd3a3d5fbe88fb67ffa65a43ba
pengu <blizzspace@gmail.com>
2024-05-14 23:58:30 -0400

    Declare output and generation functions

--- src/headers/bsdata.h
+++ src/headers/bsdata.h
@@ -1,6 +1,14 @@
 #ifndef BSDATA_H_INCLUDED
 #define BSDATA_H_INCLUDED
 
-extern int* generateUnboundedNumberOfIntegers(int numberOfIntegers);
+/* generation */
+extern void initializeGenerator();
+extern int *generateUnboundedNumberOfIntegers(int numberOfIntegers);
+extern int *generateUpperBoundedNumberOfIntegers(int numberOfIntegers, int bound);
+extern int *generateLowerBoundedNumberOfIntegers(int numberOfIntegers, int bound);
+extern int *generateBoundedNumberOfIntegers(int numberOfIntegers, int lowerBound, int upperBound);
+
+/* output */
+extern void printIntegerList(int *list, int length);
 
 #endif
