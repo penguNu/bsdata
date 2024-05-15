f1aff6d169906f52be1d389512aa494c3498e769
pengu <blizzspace@gmail.com>
2024-05-14 23:58:49 -0400

    Refactor output and generation out of main file

--- src/bsdata.c
+++ src/bsdata.c
@@ -1,34 +1,18 @@
 #include "bsdata.h"
-
 #include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-
-
-int* generateUnboundedNumberOfIntegers(int numberOfIntegers) { 
-  int *list = malloc(sizeof(int) * numberOfIntegers);
-  int iterator;
-
-  for (iterator = 0; iterator < numberOfIntegers; iterator++)
-    list[iterator] = rand();
-
-  return list;
-}
 
 int main(int argc, char** argv) {
   int numberOfRandomNumbers = 10000;
-  int *randomNumbers; 
-  int iterator;
-
-  randomNumbers = generateUnboundedNumberOfIntegers(numberOfRandomNumbers);
+  int *listOfRandomNumbers; 
 
   // seed RNG
-  srand(time(NULL));
+  initializeGenerator();
 
-  for (iterator = 0; iterator < numberOfRandomNumbers; iterator++)
-    printf("%d\n", randomNumbers[iterator]);
+  listOfRandomNumbers = generateUnboundedNumberOfIntegers(numberOfRandomNumbers);
 
-  free(randomNumbers);
+  printIntegerList(listOfRandomNumbers, numberOfRandomNumbers);
+
+  free(listOfRandomNumbers);
 
   return 0;
 }
