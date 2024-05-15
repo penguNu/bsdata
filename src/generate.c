5c20c93986ff80f12866c97d5e4060c2a786b8c7
pengu <blizzspace@gmail.com>
2024-05-15 01:31:41 -0400

    Implement separate lower and upper bound number generators

--- src/generate.c
+++ src/generate.c
@@ -1,16 +1,22 @@
 #include "generate.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>         // for seeding (srand)
+#include <limits.h>
+
+int *createEmptyList(int numberOfIntegers) {
+  return malloc(sizeof(int) * numberOfIntegers);
+}
 
 void initializeGenerator() {
   srand(time(NULL));
 }
 
-int* generateUnboundedNumberOfIntegers(int numberOfIntegers) { 
+int *generateUnboundedNumberOfIntegers(int numberOfIntegers) { 
   int *list; 
   int iterator;
 
-  list = malloc(sizeof(int) * numberOfIntegers);
+  list = createEmptyList(numberOfIntegers);
   if (!list) return NULL;
 
   for (iterator = 0; iterator < numberOfIntegers; iterator++)
@@ -19,15 +25,49 @@ int* generateUnboundedNumberOfIntegers(int numberOfIntegers) {
   return list;
 }
 
-/* nothing below here is implemented yet */
-
 int *generateUpperBoundedNumberOfIntegers(int numberOfIntegers, int bound) {
-  return NULL;
+  int *list; 
+  int iterator;
+  int randomNumber;
+
+  list = createEmptyList(numberOfIntegers);
+  if (!list) return NULL;
+
+  for (iterator = 0; iterator < numberOfIntegers; iterator++) {
+    randomNumber = rand();
+
+    if (randomNumber > bound) {
+      randomNumber = bound - (randomNumber - bound);
+    }
+
+    list[iterator] = randomNumber;
+  }
+
+  return list;
 }
 
 int *generateLowerBoundedNumberOfIntegers(int numberOfIntegers, int bound) {
-  return NULL;
+  int *list; 
+  int iterator;
+  int randomNumber;
+
+  list = createEmptyList(numberOfIntegers);
+  if (!list) return NULL;
+
+  for (iterator = 0; iterator < numberOfIntegers; iterator++) {
+    randomNumber = rand();
+
+    if (randomNumber < bound) {
+      randomNumber = bound + (bound - randomNumber);
+    }
+
+    list[iterator] = randomNumber;
+  }
+
+  return list;
 }
+
+/* nothing below here is implemented yet */
 
 int *generateBoundedNumberOfIntegers(int numberOfIntegers, int lowerBound, int upperBound) {
   return NULL;
